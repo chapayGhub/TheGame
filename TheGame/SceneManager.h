533f1f53e801cb694722ba6029bd5534b5a4b0fa
kcy1860 <ssdut.1860@gmail.com>
2012-12-11 01:55:07 -0500

    嵌入广告支持

--- TheGame/SceneManager.h
+++ TheGame/SceneManager.h
@@ -1,10 +1,12 @@
 
 #import "cocos2d.h"
 #import "CCTransition.h"
+#import "MobiSageSDK.h"
 
 @interface SceneManager : NSObject {
+
 }
 
 +(void) goPlay;  //跳转到游戏页面
-
++(MobiSageAdBanner*) getBanner;
 @end
--- TheGame/SceneManager.m
+++ TheGame/SceneManager.m
@@ -2,12 +2,26 @@
 #import "PlayBackgroundLayer.h"
 #import "PlayLayer.h"
 
+//#import "AdSageView.h"
 
 @interface SceneManager ()
+
 @end
 
 
 @implementation SceneManager
+static MobiSageAdBanner* banner;
+
++(MobiSageAdBanner*) getBanner
+{
+    if(banner == nil)
+    {
+        banner = [[MobiSageAdBanner alloc] initWithAdSize:Ad_320X50];
+        [banner setInterval:Ad_Refresh_15];
+        [banner setFrame:CGRectMake(0,430, 320, 50)];
+    }
+    return banner;
+}
 
 +(void) goPlay{
     CCDirector *director = [CCDirector sharedDirector];
@@ -16,12 +30,24 @@
     [newScene addChild:[PlayBackgroundLayer node] z:0];
     [newScene addChild:[PlayLayer node] z:0];
     
+    [SceneManager addAdBanner];
+    
     if ([director runningScene]) {
-        [director replaceScene:[CCTransitionPageTurn transitionWithDuration: 1.0f scene: newScene]];
+        [director replaceScene:[CCTransitionPageTurn transitionWithDuration: 0.5f scene: newScene]];
 	}else {
 		[director runWithScene:newScene];
 	}
 }
 
++(void) addAdBanner
+{
+     CCDirector *director = [CCDirector sharedDirector];
+    [[MobiSageManager getInstance] setPublisherID:@"ea1b5c3fa4b6434fa38b2e3d689b6169"];
+    [director.view addSubview:[SceneManager getBanner]];
+}
 
++(void) removeAdBanner
+{
+    [[SceneManager getBanner] removeFromSuperview];
+}
 @end
