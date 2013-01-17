0f18e7e9603ce3edda32f10aafb00dcb87000792
kcy1860 <ssdut.1860@gmail.com>
2013-01-16 22:19:17 -0500

    游戏名称

--- TheGame/main.m
+++ TheGame/main.m
@@ -7,10 +7,12 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "MobiSageSDK.h"
 
 int main(int argc, char *argv[]) {
     
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    [[MobiSageManager getInstance] setPublisherID:@"ea1b5c3fa4b6434fa38b2e3d689b6169"];
     int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
     [pool release];
     return retVal;
